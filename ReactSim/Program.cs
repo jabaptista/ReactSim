feba78dfb1cf3e3bcc39a3e6efe0584adc13631a
jabaptista <jabaptista@live.com.pt>
2025-11-21 00:29:56 +0000

    add http redirect

--- ReactSim/Program.cs
+++ ReactSim/Program.cs
@@ -5,6 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.HttpsPort = 8081;
+});
 
 var app = builder.Build();
 
